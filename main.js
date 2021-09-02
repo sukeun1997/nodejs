ec4a3d78135bbc5705b8212eec6c7bb2173b9085
sukeun1997 <sukeun1997@naver.com>
2021-09-02 18:26:47 +0900

    2021 / 9 / 2 nodejs

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var fs = require('fs');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = new URL('http://localhost:3000' + _url).searchParams;
-    console.log(_url);
-    console.log(queryData);
-    if(_url == '/'){
-      _url = '/index.html';
+    var title = queryData.get('id');
+
+    if(_url == '/' || _url =='/index.html'){
+      title = 'Welcome';
     }
     if(_url == '/favicon.ico'){
       response.writeHead(404);
@@ -15,7 +15,28 @@ var app = http.createServer(function(request,response){
       return;
     }
     response.writeHead(200);
-    response.end(queryData.get('id'));
+    fs.readFile(`data/${queryData.get('id')}`,'utf8',function(err,description) {
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="index.html">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+      response.end(template);
+    })
 
 });
 app.listen(3000);
