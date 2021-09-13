e3e5c16a05561c5440861539ed354ff8c48fbf11
sukeun1997 <busan-sy@naver.com>
2021-09-13 16:19:25 +0900

    node-mysql first commit

--- main.js
+++ main.js
@@ -4,8 +4,18 @@ var fs = require('fs');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var mysql = require('mysql');
 
-var app = http.createServer(function(request, response) {
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'roor',
+  password: 'tnrms2188',
+  database: 'opentutorials'
+});
+db.connect();
+
+
+var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = new URL('http://localhost:3000' + _url);
   var title = queryData.searchParams.get('id');
@@ -15,28 +25,30 @@ var app = http.createServer(function(request, response) {
     var html;
 
     if (queryData.searchParams.get('id') == null) { // 메인화면
-      fs.readdir('data', function(error, filelist) {
-        title = 'Welcome';
-        fileContent = 'Hello, Node.js';
-        html = template.html(title, template.list(filelist),
-        `<h2>${title}</h2>${fileContent}`,
-        `<a href ="/create">create</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
+      // fs.readdir('data', function(error, filelist) {
+      //   title = 'Welcome';
+      //   fileContent = 'Hello, Node.js';
+      //   html = template.html(title, template.list(filelist),
+      //   `<h2>${title}</h2>${fileContent}`,
+      //   `<a href ="/create">create</a>`
+      //   );
+      //   response.writeHead(200);
+      //   response.end(html);
+      // });
+      connection.query(`SELECT * FROM topic`, function{
+
+      })
     } else { // id 값이 있는 경우
-      fs.readdir('data', function(error, filelist) {
+      fs.readdir('data', function (error, filelist) {
         var filteredId = path.parse(queryData.searchParams.get('id')).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+        fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
           var sanitizedTitel = sanitizeHtml(title);
           var sanitizedDescription = sanitizeHtml(description, {
-            allowedTags:['h1']
+            allowedTags: ['h1']
           });
-
           html = template.html(sanitizedTitel, template.list(filelist),
-          `<h2>${sanitizedTitel}</h2>${sanitizedDescription}`,
-          `
+            `<h2>${sanitizedTitel}</h2>${sanitizedDescription}`,
+            `
           <a href ="/create">create</a>
           <a href = "/update?id=${sanitizedTitel}">update</a>
           <form action ="/process_delete" method = "post">
@@ -50,7 +62,7 @@ var app = http.createServer(function(request, response) {
       });
     }
   } else if (queryData.pathname == '/create') {
-    fs.readdir('data', function(error, filelist) {
+    fs.readdir('data', function (error, filelist) {
       title = 'WEB - CREATE';
       fileContent = 'Hello, Node.js';
       html = template.html(title, template.list(filelist),
@@ -66,34 +78,34 @@ var app = http.createServer(function(request, response) {
         <input type="submit">
       </p>
       </form>
-      `,``);
+      `, ``);
       response.writeHead(200);
       response.end(html);
     });
   } else if (queryData.pathname == '/process_create') {
     var qs = require('querystring');
     var body = '';
-    request.on('data', function(data) { // data 정보 수신
-        body = body + data;
+    request.on('data', function (data) { // data 정보 수신
+      body = body + data;
     });
-    request.on('end', function() { // data 정보 수신 종료
-        var post = qs.parse(body);
-        var title = post.title;
-        var description = post.description;
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+    request.on('end', function () { // data 정보 수신 종료
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
 
-          response.writeHead(302, {Location: `/?id=${encodeURI(title)}`}); // 302 : 리다이렉션
-          response.end();
-        });
+        response.writeHead(302, { Location: `/?id=${encodeURI(title)}` }); // 302 : 리다이렉션
+        response.end();
+      });
     });
 
   } else if (queryData.pathname == '/update') {
 
-    fs.readdir('data', function(error, filelist) {
+    fs.readdir('data', function (error, filelist) {
       var filteredId = path.parse(queryData.searchParams.get('id')).base;
-      fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+      fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
         html = template.html(title, template.list(filelist),
-        `
+          `
         <form class="" action="/process_update" method="post" >
         <input type = "hidden" name = "id" value= "${title}">
         <p>
@@ -107,7 +119,7 @@ var app = http.createServer(function(request, response) {
         </p>
         </form>
         `,
-        `
+          `
         <a href ="/create">create</a>
         <a href = "/update?id=${title}">update</a>
         `);
@@ -115,44 +127,44 @@ var app = http.createServer(function(request, response) {
         response.end(html);
       });
     });
-  } else if(queryData.pathname == '/process_update') {
+  } else if (queryData.pathname == '/process_update') {
 
     var qs = require('querystring');
     var body = '';
-    request.on('data', function(data) { // data 정보 수신
-        body = body + data;
+    request.on('data', function (data) { // data 정보 수신
+      body = body + data;
     });
-    request.on('end', function() { // data 정보 수신 종료
-        var post = qs.parse(body);
-        var id = post.id;
-        var title = post.title;
-        var description = post.description;
-        var filteredId = path.parse(id).base;
+    request.on('end', function () { // data 정보 수신 종료
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      var filteredId = path.parse(id).base;
 
-        fs.rename(`data/${filteredId}`,`data/${title}`,function(err){
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${encodeURI(title)}`}); // 302 : 리다이렉션
-            response.end();
-          });
-        });
-    });
-  } else if(queryData.pathname =='/process_delete') {
-
-    var qs = require('querystring');
-    var body = '';
-    request.on('data', function(data) { // data 정보 수신
-        body = body + data;
-    });
-    request.on('end', function() { // data 정보 수신 종료
-        var post = qs.parse(body);
-        console.log(post);
-        var id = post.id;
-        var filteredId = path.parse(id).base;
-        fs.unlink(`data/${filteredId}`, function(err) {
-          console.log(err);
-          response.writeHead(302, {Location: `/`}); // 302 : 리다이렉션
+      fs.rename(`data/${filteredId}`, `data/${title}`, function (err) {
+        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+          response.writeHead(302, { Location: `/?id=${encodeURI(title)}` }); // 302 : 리다이렉션
           response.end();
         });
+      });
+    });
+  } else if (queryData.pathname == '/process_delete') {
+
+    var qs = require('querystring');
+    var body = '';
+    request.on('data', function (data) { // data 정보 수신
+      body = body + data;
+    });
+    request.on('end', function () { // data 정보 수신 종료
+      var post = qs.parse(body);
+      console.log(post);
+      var id = post.id;
+      var filteredId = path.parse(id).base;
+      fs.unlink(`data/${filteredId}`, function (err) {
+        console.log(err);
+        response.writeHead(302, { Location: `/` }); // 302 : 리다이렉션
+        response.end();
+      });
     });
 
   } else {
